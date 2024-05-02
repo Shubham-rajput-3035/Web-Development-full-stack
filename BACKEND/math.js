709d2f38f45189c54acdc348c77c3d9137364589
Shubham-rajput-3035 <131746848+Shubham-rajput-3035@users.noreply.github.com>
2024-05-02 13:27:25 +0530

    Backend

---
+++ BACKEND/math.js
@@ -0,0 +1,39 @@
+// const sum=(a,b)=>a+b;
+// const sub=(a,b)=>a-b;
+// const mul=(a,b)=>a*b;
+// const div=(a,b)=>a/b;
+// const PI=3.14;
+// const g= 9.8;
+
+// module.exports="Hello";
+
+// let obj={
+//     sum:sum,
+//     sub:sub,
+//     mul:mul,
+//     div:div,
+//     PI:PI,
+//     g:g,
+// };
+// module.exports=obj;
+
+// module.exports.sum=(a,b)=>a+b;
+// module.exports.sub=(a,b)=>a-b;
+// module.exports.mul=(a,b)=>a*b;
+// module.exports.div=(a,b)=>a/b;
+// module.exports.PI=3.14;
+// module.exports.g= 9.8;
+
+// exports.sum=(a,b)=>a+b;
+// exports.sub=(a,b)=>a-b;
+// exports.mul=(a,b)=>a*b;
+// exports.div=(a,b)=>a/b;
+// exports.PI=3.14;
+// exports.g= 9.8;
+
+export const sum = (a, b) => a + b;
+export const sub = (a, b) => a - b;
+export const mul = (a, b) => a * b;
+export const div = (a, b) => a / b;
+export const PI = 3.14;
+export const g = 9.8;
